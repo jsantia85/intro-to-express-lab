a6269261c36babc8072426a76c9eb90d314e3cb8
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-13 18:00:34 -0400

    sets commit for being stuck

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {firstName: 'Fred', lastName: 'Jones', graduated: true, _id: 125223},
+  {firstName: 'Vic', lastName: 'Cruz', graduated: false, _id: 127904},
+  {firstName: 'Kelly', lastName: 'Smith', graduated: false, _id: 139608},
+]
+
+export { 
+	students
+}
