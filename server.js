acbd3a8700af4c4c24a426e100a7278df9b1ecbc
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-13 18:07:40 -0400

    adds final redirect

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs')
 
 // Mount routes
 app.get('/', function(req, res) {
-  res.send('<h1>Hello, friend</h1>')
+  res.redirect('/home')
 })
 
 app.get('/home', function(req, res) {
